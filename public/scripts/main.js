8c43f7f4704bfad4425c1d66a1e559a2f4247979
rhit-hartmagj <34037211+rhit-hartmagj@users.noreply.github.com>
2020-11-01 15:34:07 -0500

    history api in progress, works for two hardcoded pages

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,13 +1,49 @@
 var rhit = rhit || {};
 
+
+//this is set up for localhost stuff, switch it when you actually deploy
+rhit.swapPage = function(href) {
+	var req = new XMLHttpRequest();
+	req.open("GET",
+			 "http://localhost:5000/" + href,
+			 false);
+	req.send(null);
+	if (req.status == 200) {
+		document.getElementById("pageHolder").innerHTML = req.responseText;
+		rhit.setupHistoryClicks();
+		return true;
+	  }
+	  return false;
+}
+
+rhit.addClicker = function(link) {
+	link.addEventListener("click", function(e) {
+	  rhit.swapPage(link.dataset.link);
+	  history.pushState(null, null, link.dataset.link);
+	  e.preventDefault();
+	}, false);
+}
+
+rhit.setupHistoryClicks = function() {
+	//make this a class instead of an id, so we can have many of these page changers passed through. make sure they all have a data-link value though
+	//let loginBtn = document.getElementById("login");
+	rhit.addClicker(document.getElementById("login"));
+	//console.log(loginBtn.dataset.link);
+}
+
 rhit.main = function () {
 	console.log("Ready");
 
+	rhit.setupHistoryClicks();
+
+	window.addEventListener("popstate", function(e) {
+		rhit.swapPage(location.pathname);
+	});
+
 	//for testing purposes
-	document.querySelector("#login").onclick = (event) => {
-		console.log("CLICKED");
-		window.location.href = "/playmode.html";
-	};
+	// document.querySelector("#login").onclick = (event) => {
+	// 	window.location.href = "/playmode.html";
+	// };
 };
 
 rhit.main();
